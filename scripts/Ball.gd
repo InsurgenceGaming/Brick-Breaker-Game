d363930bd942f2242e552de00d175cd9de41e220
InsurgenceGaming <153716630+InsurgenceGaming@users.noreply.github.com>
2024-03-30 03:20:06 +0000

    Possibly fixed ball colliding with paddle

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -16,7 +16,12 @@ func _physics_process(delta):
 	if (!collision):
 		return
 	velocity = velocity.bounce(collision.get_normal())
-	
+	if collision.get_collider().is_in_group("paddle") && collision.get_angle() > 0:
+		velocity = velocity.bounce(collision.get_normal())
+		velocity.y = -.8
+		collision_layer = 0 
+		collision_mask = 0 
+		# add a timer here and when the timer runs out reset the collision layer/mask to the correct settings
 	if collision.get_collider().is_in_group("Brick"):
 		collision.get_collider().Damage(1)
 
