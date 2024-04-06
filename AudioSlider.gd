7c235706746d9681eed3953b48d31782daad0b30
InsurgenceGaming <monstergaming@outlook.com>
2024-04-06 16:55:13 +0100

    Audio fixes
    Audio bar now has text box added and a text value

--- AudioSlider.gd
+++ AudioSlider.gd
@@ -1,17 +1,29 @@
 extends HSlider
-
+@export var spinbox = SpinBox
 @export
 var bus_name : String
 var bus_index : int
 
-func ready():
+func _ready():
 	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
-
-
+	value = db_to_linear(
+		AudioServer.get_bus_volume_db(bus_index)
+	)
+	print(value)
 
 func _on_value_changed(value):
-	print(value)
 	AudioServer.set_bus_volume_db(
 	bus_index,
 	linear_to_db(value))
+	spinbox.value = db_to_linear(
+		AudioServer.get_bus_volume_db(bus_index)
+	)*100
+
+
+func _on_spin_box_value_changed(value):
+	AudioServer.set_bus_volume_db(
+		bus_index,
+		linear_to_db(value/100)
+	)
+	self.value = value/100
