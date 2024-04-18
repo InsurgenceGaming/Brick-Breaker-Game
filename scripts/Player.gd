b5324aebbb8893be6dd64d0addf757b7031a4c1a
InsurgenceGaming <monstergaming@outlook.com>
2024-04-18 20:01:12 +0100

    addfed gameover buttons

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 class_name Paddle
-
+@export var RestartButton : Button
+@export var MainMenuButton : Button
+@export var RestartLevel : Button
 @export var Lives_HUD : Label
 var Lives : int = 3
 var camera_rect : Rect2
@@ -12,11 +14,16 @@ var direction = Vector2.ZERO
 var Ball_spawned :bool = false
 var lock_y
 @onready var ball = $"../Ball" as Ball
-
+var gameover = false
 var fire_ball : bool = false
+var Buttons = []
 
 
 func _ready():
+	RestartButton.disabled = true
+	MainMenuButton.disabled = true
+	RestartLevel.disabled = true
+	Buttons = [RestartButton,MainMenuButton,RestartLevel]
 	Lives_HUD.text = str("Lives: ", Lives)
 	lock_y = position.y
 	camera_rect = camera.get_viewport_rect()
@@ -34,17 +41,18 @@ func _process(delta):
 		global_position.x = camera_end_x - halved_paddle
 	
 func _physics_process(delta):
-	if Input.is_action_pressed("Paddle_move_left"):
-		velocity = Vector2.LEFT * SPEED
-	elif  Input.is_action_pressed("Paddle_move_right"):
-		velocity = Vector2.RIGHT  * SPEED
-	else:
-		velocity = Vector2.ZERO
-	if velocity != Vector2.ZERO and !Ball_spawned and is_instance_valid(ball):
-		ball.start_ball()
-		Ball_spawned = true
-		print(Ball_spawned)
-	move_and_slide()
+	if !gameover:
+		if Input.is_action_pressed("Paddle_move_left"):
+			velocity = Vector2.LEFT * SPEED
+		elif  Input.is_action_pressed("Paddle_move_right"):
+			velocity = Vector2.RIGHT  * SPEED
+		else:
+			velocity = Vector2.ZERO
+		if velocity != Vector2.ZERO and !Ball_spawned and is_instance_valid(ball):
+			ball.start_ball()
+			Ball_spawned = true
+			print(Ball_spawned)
+		move_and_slide()
 
 func Lives_added():
 	Lives += 1
@@ -53,8 +61,20 @@ func Lives_added():
 
 func Lives_lost(lost_lives):
 	if Lives == 0:
-		await get_tree().create_timer(5.0).timeout
-		get_tree().quit()
+		gameover = true
+		var my_tween = create_tween()
+		my_tween.set_parallel(true)
+		print(Buttons)
+		for button in Buttons:
+			if button.name == "Quit":
+				my_tween.tween_property(button,"position:x",-38,1)
+			else:
+				my_tween.tween_property(button,"position:x",-48,1)
+			
+		await get_tree().create_timer(1.0).timeout
+		for button in Buttons:
+			button.disabled = false
+		
 	else: 
 		Lives -= lost_lives
 		Ball_spawned = false
@@ -90,9 +110,11 @@ func New_stage():
 
 	 
 
-var debug_mode : bool = false
+var debug_mode : bool = true
 @export var new_ball : PackedScene
 func _input(event):
+	if Input.is_key_pressed(KEY_P) and debug_mode:
+		Lives = 0
 	if Input.is_key_pressed(KEY_SPACE) and debug_mode:
 		for i in 999:
 			var ball = new_ball.instantiate()
@@ -102,12 +124,32 @@ func _input(event):
 			ball.position = position
 			ball.position.y = 282
 			print("ball should be here")
+			
+func _on_back_pressed():
+	get_tree().quit()
 
 
+func _on_main_menu_pressed():
+	var my_tween = create_tween()
+	my_tween.set_parallel(true)
+	for button in Buttons:
+		if button.name == "Quit":
+			my_tween.tween_property(button,"position:x",-671,1)
+		else:
+			my_tween.tween_property(button,"position:x",-684,1)
+		button.disabled = true
+	await get_tree().create_timer(1.0).timeout
+	get_tree().change_scene_to_file("res://Scenes/Main Menu.tscn")
 
 
-
-
-
-func _on_body_entered(body):
-	print(body)
+func _on_restart_pressed():
+	var my_tween = create_tween()
+	my_tween.set_parallel(true)
+	for button in Buttons:
+		if button.name == "Quit":
+			my_tween.tween_property(button,"position:x",-671,1)
+		else:
+			my_tween.tween_property(button,"position:x",-684,1)
+		button.disabled = true
+	await get_tree().create_timer(1.0).timeout
+	get_tree().change_scene_to_file("res://Snake.tscn")
